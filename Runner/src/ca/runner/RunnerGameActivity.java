a823e0596056b396f3c06081f8e612fc3cb45b2c
Johnnie <johnnie-l@live.ca>
2013-12-22 23:12:34 -0800

    Background Music Added
    - Background music added
    - Non-looping (NEED TO ADD LOOPABLE)

--- Runner/src/ca/runner/RunnerGameActivity.java
+++ Runner/src/ca/runner/RunnerGameActivity.java
@@ -1,16 +1,20 @@
 package ca.runner;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class RunnerGameActivity extends Activity {
 	private GameView gameView;
+	private MediaPlayer bGMusic;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		gameView = new GameView(this);
 		setContentView(gameView);
+		bGMusic = MediaPlayer.create(this, R.raw.gamemusic);
+		bGMusic.start();
 	}
 
 	@Override
