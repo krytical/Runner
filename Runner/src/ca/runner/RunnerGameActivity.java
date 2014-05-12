6f04a03960f5efcf9b5d1e6b54fa0ae43296162d
iellorin <iellorin@hotmail.com>
2014-05-12 11:14:17 -0700

    music fix
    i think it's fixed

--- Runner/src/ca/runner/RunnerGameActivity.java
+++ Runner/src/ca/runner/RunnerGameActivity.java
@@ -9,22 +9,28 @@ import android.os.Bundle;
 
 public class RunnerGameActivity extends Activity {
 	private GameView gameView;
+	private MediaPlayer bGMusic;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		gameView = new GameView(this);
 		setContentView(gameView);
+		bGMusic = MediaPlayer.create(this, R.raw.game_music);
+		if (!bGMusic.isPlaying()) {
+			bGMusic.start();
+			bGMusic.setLooping(true);
+		}
 	}
 
-//	@Override
-//	public void onPause(){
-//		super.onPause();
-//		gameView.gameLoop.runner = false;
-//		ArrayList<MediaPlayer> soundsToRelease = gameView.getSounds();
-//		for(MediaPlayer sound : soundsToRelease){
-//			sound.release();
-//		}
-//		finish();
-//	}
+	@Override
+	public void onPause(){
+		super.onPause();
+		// gameView.gameLoop.runner = false;
+		ArrayList<MediaPlayer> soundsToRelease = gameView.getSounds();
+		for(MediaPlayer sound : soundsToRelease){
+			sound.release();
+		}
+		finish();
+	}
 }
